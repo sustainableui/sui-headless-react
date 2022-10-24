1f77ffa0aa331fd88e3d47a7595120f8b1d1f8b5
Samuel Pitoňák <sampittko@gmail.com>
2022-10-24 20:30:27 +0200

    refactor: Sui

--- src/base/context/sui.jsx
+++ src/base/context/sui.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 
 const SuiContext = React.createContext(undefined);
-SuiContext.displayName = 'SUIContext';
+SuiContext.displayName = 'SuiContext';
 
 function useSuiContext() {
   return useContext(SuiContext);
